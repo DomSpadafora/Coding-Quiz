00f0429b35401f8362f81f74e27153b37bed11ac
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-04-05 22:38:08 -0400

    handlequestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,28 @@ let QIndex = 0;
 let time = 60;
 let score = 0;
 
+let questions = [
+    {
+        Q: 'What is the key word to create a variable?',
+        A: ['HTML', 'CSS', 'var', 'flexbox'],
+        C: 'var',
+    },
+    {
+        Q: 'Which coding language is considered the foundation/structure of a webpage?',
+        A: ['HTML', 'CSS', 'JavaScript', 'Python'],
+        C: 'HTML',
+    }, 
+    {
+        Q: 'A container object that holds a fixed number of values of a single type is?',
+        A: ['array', 'object', 'string', 'boolean'],
+        C: 'array',
+    }, 
+    {
+        Q: 'Which primitive type is an expression that evaluates to be either true or false?',
+        A: ['number', 'string', 'boolean', 'undefined'],
+        C: 'boolean',
+    }
+    ];
 
 const startTimer = () => {
     time--;
@@ -12,3 +34,20 @@ const startTimer = () => {
         endQuiz();
     }
 };
+
+const handleQuestions = () => {
+
+}
+
+const beginQuiz = () => {
+    console.log('you clicked me')
+    questions();
+    clockId = setInterval(startTimer, 1000)
+
+}
+
+
+
+
+
+start.addEventListener('click', beginQuiz)
